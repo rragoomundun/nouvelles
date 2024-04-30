d3c1964dd80f330915346491bd7374365e533839
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-04-30 15:04:04 +0400

    Get article

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { DateTime } from 'luxon';
 
 import { NotFoundComponent } from '../not-found/not-found.component';
 
 import { ArticleService } from '../../shared/services/article/article.service';
+import { DateService } from '../../shared/services/date/date.service';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [RouterLink, NotFoundComponent],
+  imports: [RouterLink, TranslateModule, NotFoundComponent],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss',
 })
@@ -17,14 +21,15 @@ export class ArticleComponent implements OnInit {
   articleId: number;
   title: string;
   image: string;
-  date: Date;
-  updatedDate: Date;
+  date: string;
+  updatedDate: string;
   author: any;
   content: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private articleService: ArticleService,
+    private dateService: DateService,
   ) {}
 
   ngOnInit() {
@@ -38,13 +43,28 @@ export class ArticleComponent implements OnInit {
 
         this.title = title;
         this.image = image;
-        this.date = date;
-        this.updatedDate = updatedDate;
         this.author = author;
         this.content = content;
 
-        console.log(this.author);
-        console.log(this.date);
+        const dateObj = new Date(date);
+        this.date = this.dateService.dt
+          .set({
+            day: dateObj.getDate(),
+            month: dateObj.getMonth() + 1,
+            year: dateObj.getFullYear(),
+          })
+          .toLocaleString(DateTime.DATE_FULL);
+
+        if (updatedDate) {
+          const updatedDateObj = new Date(updatedDate);
+          this.updatedDate = this.dateService.dt
+            .set({
+              day: updatedDateObj.getDate(),
+              month: updatedDateObj.getMonth() + 1,
+              year: updatedDateObj.getFullYear(),
+            })
+            .toLocaleString(DateTime.DATE_FULL);
+        }
       },
       error: (value) => {
         if (value.status === 404) {
