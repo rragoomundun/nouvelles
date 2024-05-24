93d6b9849c11f721c0759b6ff6e6d333dba0e194
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-05-24 18:04:44 +0400

    New discussion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,6 +16,7 @@ import { ForumComponent } from './components/forum/forum.component';
 import { ForumListComponent } from './components/forum-list/forum-list.component';
 import { DiscussionsComponent } from './components/discussions/discussions.component';
 import { DiscussionComponent } from './components/discussion/discussion.component';
+import { NewDiscussionComponent } from './components/new-discussion/new-discussion.component';
 
 export const routes: Routes = [
   {
@@ -82,11 +83,22 @@ export const routes: Routes = [
       },
       {
         path: ':forum',
-        component: DiscussionsComponent,
-      },
-      {
-        path: ':forum/discussion/:discussionId/:discussionTitle',
-        component: DiscussionComponent,
+        children: [
+          {
+            path: '',
+            component: DiscussionsComponent,
+          },
+          {
+            path: 'discussion',
+            children: [
+              { path: 'nouvelle', component: NewDiscussionComponent },
+              {
+                path: ':discussionId/:discussionTitle',
+                component: DiscussionComponent,
+              },
+            ],
+          },
+        ],
       },
     ],
   },
---
+++ src/app/components/new-discussion/new-discussion.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewDiscussionComponent } from './new-discussion.component';
+
+describe('NewDiscussionComponent', () => {
+  let component: NewDiscussionComponent;
+  let fixture: ComponentFixture<NewDiscussionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NewDiscussionComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(NewDiscussionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
